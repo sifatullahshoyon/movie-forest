8fefeb12ca28146bc36cd516d5f81b20d951e556
freelancersifat627e <freelancersifat627@gmail.comm>
2023-11-19 16:11:31 +0600

    showing 9th data on UI when click see more button all data to show in UI

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,16 +1,42 @@
-import React, { useEffect, useState } from 'react';
-import { useLoaderData } from 'react-router-dom';
-import Movie from './Movie';
-
+import React, { useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import Movie from "./Movie";
 
 const Movies = () => {
-    const movies = useLoaderData();
-    console.log(movies);
-    return (
-        <div className='grid-container'>
-            {movies?.map(movie => <Movie movie={movie} key={movie.id} />)}
-        </div>
-    );
+  const movies = useLoaderData();
+  const [sliceData, setSliceData] = useState(false);
+  const itemToShowAll = sliceData ? movies : movies.slice(0, 9);
+  const handleSeeMoreBtn = () => {
+    setSliceData(true);
+  };
+  const handleSeeLessBtn = () => {
+    setSliceData(!true);
+  };
+  return (
+    <div>
+      <div className="grid-container">
+        {itemToShowAll?.map((movie) => (
+          <Movie key={movie.id} movie={movie} />
+        ))}
+      </div>
+      <div className="text-center mb-5">
+        {!sliceData && (
+          <button
+            id="seeMore-btn"
+            onClick={handleSeeMoreBtn}
+            className="btn btn-primary visible"
+          >
+            See More
+          </button>
+        )}
+        {sliceData && (
+          <button onClick={handleSeeLessBtn} className="btn btn-primary">
+            Show Less
+          </button>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Movies;
